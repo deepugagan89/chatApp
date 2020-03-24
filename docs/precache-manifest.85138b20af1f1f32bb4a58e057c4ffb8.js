9181f704b85b18c3e71c129ec52351b3ba600921
Gagandeep Singh <deepugagan89@yahoo.com>
2020-03-24 23:04:26 +0530

    added docs folder for pages

---
+++ docs/precache-manifest.85138b20af1f1f32bb4a58e057c4ffb8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14dc3a4d6739d6491956ef0646c963fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72f66d159336bcc3508f",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "0840a12548e7d71dae4e",
+    "url": "/static/css/main.8e79d063.chunk.css"
+  },
+  {
+    "revision": "72f66d159336bcc3508f",
+    "url": "/static/js/2.a092f81a.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/static/js/2.a092f81a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0840a12548e7d71dae4e",
+    "url": "/static/js/main.17a5d70d.chunk.js"
+  },
+  {
+    "revision": "4acb8efb8e3fca718544",
+    "url": "/static/js/runtime-main.b2977abe.js"
+  }
+]);
